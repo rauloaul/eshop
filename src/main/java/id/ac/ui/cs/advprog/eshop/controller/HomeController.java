336e7a9851a101f16a66510d09dd0bd0052ab31e
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-02-06 23:53:31 +0700

    add controller home

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -1,2 +1,13 @@
-package id.ac.ui.cs.advprog.eshop.controller;public class HomeController {
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "homePage";
+    }
 }
