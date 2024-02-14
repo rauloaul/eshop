4ca7536c65ced6d0d1f83d0c6bede0eab5fe567a
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-02-14 13:36:17 +0700

    Add HomeController Test

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
@@ -1,2 +1,27 @@
-package id.ac.ui.cs.advprog.eshop.controller;public class HomeControllerTEst {
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = HomeController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomeController() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(view().name("homePage"));
+    }
 }
