ac5d0a6d5967d68a5928adef5f06cbf84157357e
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-02-24 16:39:31 +0700

    Fix typo in Car Service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface CarService {
     public Car create(Car car);
-    public List<Car> findall();
+    public List<Car> findAll();
     Car findById(String carId);
     public void update(String carId, Car car);
     public void deleteCarById(String carId);
