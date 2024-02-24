3db176bdf1505e54051bc2f6554bd41d807d2c87
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-02-24 18:36:24 +0700

    OCP - edit create, only replace product id, if not given

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -18,7 +18,9 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product create(Product product) {
-        product.setProductId(String.valueOf(UUID.randomUUID()));
+        if (product.getProductId() == null) {
+            product.setProductId(String.valueOf(UUID.randomUUID()));
+        }
         productRepository.create(product);
         return product;
     }
