2c7f5ea2da9afaade7f79c77666f50ff8536c39f
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-03-06 20:54:22 +0700

    [RED] Add Payment repository skeleton

---
+++ src/main/java/repository/PaymentRepository.java
@@ -0,0 +1,19 @@
+package repository;
+
+import model.Payment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentRepository {
+    private List<Payment> paymentList = new ArrayList<>();
+    public Payment save(Payment payment) {
+        return null;
+    }
+    public Payment findById(String id) {
+        return null;
+    }
+    public List<Payment> findAll() {
+        return null;
+    }
+}
