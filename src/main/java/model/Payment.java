0afad95ceeea9e45a94714f201301de9d181be08
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-03-06 20:18:05 +0700

    [GREEN] Implement Payment model

--- src/main/java/model/Payment.java
+++ src/main/java/model/Payment.java
@@ -13,8 +13,28 @@ public class Payment {
     String status;
     Map<String, String> paymentData;
 
-    public Payment(String id, String method,Map<String, String> paymentData) {
+    public Payment(String id, String method,Map<String, String> paymentData){
+        this.id = id;
+        this.setMethod(method);
+        this.status = "CHECKING_PAYMENT";
+        this.paymentData = paymentData;
     }
-    public Payment(String id, String method,String status, Map<String, String> paymentData) {
+    public Payment(String id, String method,String status, Map<String, String> paymentData){
+        this(id, method, paymentData);
+        this.setStatus(status);
+    }
+    public void setStatus(String status) {
+        if (status.equals("SUCCESS") || status.equals("REJECTED")){
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void setMethod(String method) {
+        if (method.equals("CASH") || method.equals("VOUCHER")){
+            this.method = method;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
