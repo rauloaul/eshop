62643553fb0ae328f47bbfb2b0a9669d122e3fc2
Rafif Firmansyah <rafif.firmansyah@gmail.com>
2024-03-06 20:15:03 +0700

    [RED] Add Payment model skeleton

--- src/main/java/model/Payment.java
+++ src/main/java/model/Payment.java
@@ -1,2 +1,20 @@
-package model;public class Payment {
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+public class Payment {
+    String id;
+    String method;
+    @Setter
+    String status;
+    Map<String, String> paymentData;
+
+    public Payment(String id, String method,Map<String, String> paymentData) {
+    }
+    public Payment(String id, String method,String status, Map<String, String> paymentData) {
+    }
 }
